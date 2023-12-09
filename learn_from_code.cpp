a945c72c7d84502a86c80e14f3371add9572994a
Beatrix-0 <mdsiam9837@gmail.com>
2023-12-10 01:24:59 +0600

    code

---
+++ learn_from_code.cpp
@@ -0,0 +1,39 @@
+//Grid :
+
+1. https://codeforces.com/contest/1846/problem/B
+2. https://codeforces.com/problemset/problem/1850/C
+3. https://codeforces.com/problemset/problem/1862/A
+
+
+// Number Theory 
+
+--> Hermonic Series
+1. https://cses.fi/problemset/task/1713
+2. https://atcoder.jp/contests/abc172/tasks/abc172_d
+--> Primality Test
+1. https://www.codechef.com/problems/PRB01?tab=statement
+--> Sieve of Eratosthenes
+
+
+// STL
+
+--> Pair 
+1. https://codeforces.com/problemset/problem/1867/A
+2. 
+
+// Bitwise 
+1. https://codeforces.com/problemset/problem/1869/A (https://shorturl.at/hFMQ3)
+
+//Random 
+1. https://codeforces.com/problemset/problem/1856/B
+2. https://www.hackerearth.com/problem/algorithm/monk-and-rotation-3-bcf1aefe/
+3. https://codeforces.com/problemset/problem/1891/C (https://t.ly/9P0DU)
+4. https://codeforces.com/contest/1899/problem/C (https://shorturl.at/fswR4) [Kadnes Algorithm]
+5. https://codeforces.com/contest/1899/problem/E (https://shorturl.at/rGMV2)
+6. https://codeforces.com/contest/1873/problem/G (https://shorturl.at/dluvw)
+7. https://codeforces.com/problemset/problem/1861/B
+8. https://codeforces.com/problemset/problem/1900/B(https://youtu.be/wYeYUoYv5pk?feature=shared)
+9. https://codeforces.com/contest/1901/problem/B (https://youtu.be/X-afl8CGtqs?feature=shared)
+10.https://www.spoj.com/problems/TDKPRIME/ ()
+11. https://codeforces.com/problemset/problem/1907/C(https://youtu.be/mfixApfLdZc?feature=shared)
+12. https://codeforces.com/contest/1883/problem/C (https://youtu.be/Wn_0TEf5rOc?feature=shared)
